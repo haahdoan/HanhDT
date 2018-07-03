f3a0d3e424d6c2641dc178a924a4b30fc76772ae
Hanh Doan <haahdoan@gmail.com>
2018-07-03 15:23:39 +0700

    Log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,6 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+
+  include SessionsHelper
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = t ".welcome"
       redirect_to @user
     else
@@ -14,11 +15,11 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
-    if @user.nil?
-      flash[:danger] = t ".user"
-      redirect_to root_path
-    end
+    @user = User.find_by id: params[:id]
+
+    return if @user
+    flash[:danger] = t ".user"
+    redirect_to root_path
   end
 
   private
